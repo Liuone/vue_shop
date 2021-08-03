07056413252a6ad47bfc511e4def70386de1fd68
Liuone <liuyi80028@outlook.com>
2021-08-03 11:43:46 +0800

    finfish order

--- src/router/index.js
+++ src/router/index.js
@@ -10,6 +10,7 @@ import Cate from '../components/goods/Cate.vue'
 import Params from '../components/goods/Params.vue'
 import List from '../components/goods/List.vue'
 import Add from '../components/goods/Add.vue'
+import Order from '../components/order/Order.vue'
 Vue.use(VueRouter)
 
 const routes = [{
@@ -53,6 +54,10 @@ const routes = [{
         {
             path: '/goods/add',
             component: Add
+        },
+        {
+            path: '/orders',
+            component: Order
         }
     ]
 }]

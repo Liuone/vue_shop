9741750574cb00e9ff52ab56594a29dec7de5410
Liuone <liuyi80028@outlook.com>
2021-08-06 15:52:51 +0800

    finish report

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,7 @@ import Params from '../components/goods/Params.vue'
 import List from '../components/goods/List.vue'
 import Add from '../components/goods/Add.vue'
 import Order from '../components/order/Order.vue'
+import Report from '../components/report/Report.vue'
 Vue.use(VueRouter)
 
 const routes = [{
@@ -58,6 +59,10 @@ const routes = [{
         {
             path: '/orders',
             component: Order
+        },
+        {
+            path: '/reports',
+            component: Report
         }
     ]
 }]
